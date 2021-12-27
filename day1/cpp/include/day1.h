86ee521f5ac1d3aec3a5dbe420f89aa5afc78477
XiaoTonyLuo <ssx2786@gmail.com>
2021-12-27 12:48:10 +0800

    add cpp code for day1

---
+++ day1/cpp/include/day1.h
@@ -0,0 +1,21 @@
+﻿#include <fstream>
+#include <vector>
+#include <iterator>
+
+using std::ifstream;
+using std::vector;
+
+size_t Calculate(ifstream& fin)
+{
+    vector<int> data{};
+    std::copy(std::istream_iterator<int>(fin), std::istream_iterator<int>(), std::back_inserter(data));
+    size_t counter = 0;
+    for (size_t i = 1; i < data.size(); ++i)
+    {
+        if (data[i] > data[i - 1])
+        {
+            ++counter;
+        }
+    }
+    return counter;
+}
---
+++ day1/cpp/src/day1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include "day1.h"
+
+using std::ifstream;
+using std::string;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        std::cerr << "Arguments number error! Need 1 argument, given " << argc - 1 << " argument(s)!";
+        return -1;
+    }
+    string arg = argv[1];
+    arg = "..\\..\\..\\..\\" + arg;
+
+    ifstream fin;
+    fin.open(arg);
+    if (!fin)
+    {
+        std::cerr << "cannot open the file";
+        return -1;
+    }
+
+    auto counter = Calculate(fin);
+    std::cout << "The measurement incresed " << counter << " times!";
+}
